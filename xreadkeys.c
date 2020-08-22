7c56a3cb07cc4aa89649e71857164e4e1054373f
Francis Nguyen <francis@ngyn.me>
2020-08-22 00:23:29 -0600

    Fix style

--- xreadkeys.c
+++ xreadkeys.c
@@ -5,48 +5,52 @@
 #include "scancodes.h"
 #include <X11/XKBlib.h>
 
-enum params {//argv-indices:
-	P_EXE, //executable name
-	P_DEV, //device file
-	P_LAY, //layout
-	P_UNI, //unicode method
-	NUM_P  //number of parameters
+enum params
+{          //argv-indices:
+    P_EXE, //executable name
+    P_DEV, //device file
+    P_LAY, //layout
+    P_UNI, //unicode method
+    NUM_P  //number of parameters
 };
 
-enum uni_m {//unicode methods:
-	SKIP, //ignore any keys not on the layout
-	GTK_HOLD, //hold ctrl and shift while entering hex values
-	GTK_SPACE, //end hex sequence with spacebar
-	WINDOWS //use alt+numpad
+enum uni_m
+{              //unicode methods:
+    SKIP,      //ignore any keys not on the layout
+    GTK_HOLD,  //hold ctrl and shift while entering hex values
+    GTK_SPACE, //end hex sequence with spacebar
+    WINDOWS    //use alt+numpad
 };
-enum errors {
-	ERR_SUCCESS, //no error
-	ERR_ARGCOUNT, //wrong number of arguments
-	ERR_SYMBOL, //symbol not in look up table
-	ERR_LAYOUT, //parameter P_LAY does not contain a correct keyboard layout
-	ERR_LAZY //i haven't done this
-};
-
-void send_key (FILE* hid_dev, unsigned short key, unsigned short mod);
-enum errors send_unicode (FILE* hid_dev, unsigned int unicode, enum uni_m method, enum kbdl layout);
-
-int main(int argc, char** argv)
+enum errors
 {
-    if (argc != NUM_P) {
-		fprintf (stderr, "Usage: %s <device file> <layout> <unicode>\n", argv[P_EXE]);
-		fprintf (stderr, "Takes string to type from stdin\n");
-		fprintf (stderr, "<device file>:\ton the Raspberry Pi usually /dev/hidg0\n");
-		fprintf (stderr, "<layout>:\n\t%d\t%s\n\t%d\t%s\n\t%d\t%s\n", 
-			en_US, "en_US", 
-			de_AT, "de_AT (w/ dead keys)", 
-			de_ND, "de_AT-nodeadkeys");
-		fprintf (stderr, "<unicode>:\n\t%d\t%s\n\t%d\t%s\n\t%d\t%s\n\t%d\t%s\n", 
-			SKIP, "skip over unicode characters", 
-			GTK_HOLD, "X11 Holddown: CTRL+SHIFT+[u, hex]",
-			GTK_SPACE, "X11 Space: CTRL+SHIFT+u, hex, SPACE",
-			WINDOWS, "Windows: Alt+[Numpad]");
-		return ERR_ARGCOUNT;
-	}
+    ERR_SUCCESS,  //no error
+    ERR_ARGCOUNT, //wrong number of arguments
+    ERR_SYMBOL,   //symbol not in look up table
+    ERR_LAYOUT,   //parameter P_LAY does not contain a correct keyboard layout
+    ERR_LAZY      //i haven't done this
+};
+
+void send_key(FILE *hid_dev, unsigned short key, unsigned short mod);
+enum errors send_unicode(FILE *hid_dev, unsigned int unicode, enum uni_m method, enum kbdl layout);
+
+int main(int argc, char **argv)
+{
+    if (argc != NUM_P)
+    {
+        fprintf(stderr, "Usage: %s <device file> <layout> <unicode>\n", argv[P_EXE]);
+        fprintf(stderr, "Takes string to type from stdin\n");
+        fprintf(stderr, "<device file>:\ton the Raspberry Pi usually /dev/hidg0\n");
+        fprintf(stderr, "<layout>:\n\t%d\t%s\n\t%d\t%s\n\t%d\t%s\n",
+                en_US, "en_US",
+                de_AT, "de_AT (w/ dead keys)",
+                de_ND, "de_AT-nodeadkeys");
+        fprintf(stderr, "<unicode>:\n\t%d\t%s\n\t%d\t%s\n\t%d\t%s\n\t%d\t%s\n",
+                SKIP, "skip over unicode characters",
+                GTK_HOLD, "X11 Holddown: CTRL+SHIFT+[u, hex]",
+                GTK_SPACE, "X11 Space: CTRL+SHIFT+u, hex, SPACE",
+                WINDOWS, "Windows: Alt+[Numpad]");
+        return ERR_ARGCOUNT;
+    }
 
     Display *display;
     Window window;
@@ -57,7 +61,6 @@ int main(int argc, char** argv)
     printf("Starting...\n");
     printf("Opening %s\n", argv[P_DEV]);
 
- 
     /* open connection with the server */
     display = XOpenDisplay(NULL);
     if (display == NULL)
@@ -65,7 +68,7 @@ int main(int argc, char** argv)
         fprintf(stderr, "Cannot open display\n");
         exit(1);
     }
- 
+
     s = DefaultScreen(display);
 
     /* get the display width and height */
@@ -74,11 +77,11 @@ int main(int argc, char** argv)
 
     /* create fullscreen window */
     window = XCreateSimpleWindow(display, RootWindow(display, s), 0, 0, width, height, 1,
-                           BlackPixel(display, s), WhitePixel(display, s));
- 
+                                 BlackPixel(display, s), WhitePixel(display, s));
+
     /* select kind of events we are interested in */
-    XSelectInput(display, window, KeyPressMask | KeyReleaseMask | EnterWindowMask | LeaveWindowMask );
- 
+    XSelectInput(display, window, KeyPressMask | KeyReleaseMask | EnterWindowMask | LeaveWindowMask);
+
     /* map (show) the window */
     XMapWindow(display, window);
 
@@ -89,55 +92,58 @@ int main(int argc, char** argv)
     {
         XNextEvent(display, &event);
 
-        FILE* hid_dev = NULL;
+        FILE *hid_dev = NULL;
         if (event.type == KeyPress || event.type == KeyRelease)
-	{
-            hid_dev = fopen (argv[P_DEV], "w");
-	}
+        {
+            hid_dev = fopen(argv[P_DEV], "w");
+        }
 
         /* keyboard events */
         if (event.type == KeyPress)
         {
             KeySym keysym = XLookupKeysym(&event.xkey, 0);
-	    const char space[] = " ";
-	    const char* test = &space[0];
+            const char space[] = " ";
+            const char *test = &space[0];
 
-            char* sym_name = NULL;
+            char *sym_name = NULL;
             if (event.xkey.keycode == 0x41)
-	    {
-	        sym_name = test;
-	    }
-	    else
-	    {
-	        sym_name =  XKeysymToString(keysym);
-	    }
+            {
+                sym_name = test;
+            }
+            else
+            {
+                sym_name = XKeysymToString(keysym);
+            }
 
-            printf( "KeyPress: %s [0x%x]\n", sym_name, keysym);
+            printf("KeyPress: %s [0x%x]\n", sym_name, keysym);
 
-            struct keysym* s = toscan2 (keysym);
-	    if (s == NULL) {
-		printf ("Key symbol not found.\n");
-		continue;
-	    }
-
-            struct layout* l = tolay (s, atoi (argv[P_LAY]));
-	    if (l == NULL) {
-			    fprintf (stderr, "Unrecognised keyboard layout.\n");
+            struct keysym *s = toscan2(keysym);
+            if (s == NULL)
+            {
+                printf("Key symbol not found.\n");
                 continue;
             }
-    
-//            if (l->key != 0x00) {
+
+            struct layout *l = tolay(s, atoi(argv[P_LAY]));
+            if (l == NULL)
+            {
+                fprintf(stderr, "Unrecognised keyboard layout.\n");
+                continue;
+            }
+
+            //            if (l->key != 0x00) {
+            send_key(hid_dev, l->key, l->mod);
+            send_key(hid_dev, '\0', '\0'); //release all keys
+            if (l->is_dead)
+            {
+                //dead keys need to be pressed twice to show up
                 send_key(hid_dev, l->key, l->mod);
                 send_key(hid_dev, '\0', '\0'); //release all keys
-                if (l->is_dead) {
-                    //dead keys need to be pressed twice to show up
-                    send_key(hid_dev, l->key, l->mod);
-                    send_key(hid_dev, '\0', '\0'); //release all keys
-                }
-//            }
+            }
+            //            }
 
             /* exit on ESC key press */
-            if ( event.xkey.keycode == 0x09 )
+            if (event.xkey.keycode == 0x09)
                 break;
         }
         else if (event.type == KeyRelease)
@@ -151,81 +157,85 @@ int main(int argc, char** argv)
         }
         else if (event.type == EnterNotify && eventLoopRunOnce)
         {
-        printf("EnterWindow\n");
+            printf("EnterWindow\n");
         }
         else if (event.type == LeaveNotify)
         {
             printf("LeaveWindow\n");
         }
 
-	if (hid_dev != NULL)
-	{
-	    fclose (hid_dev);
-	}
+        if (hid_dev != NULL)
+        {
+            fclose(hid_dev);
+        }
     }
-
 
     /* close connection to server */
     XCloseDisplay(display);
- 
+
     return 0;
 }
 
-
-void send_key (FILE* hid_dev, unsigned short key, unsigned short mod) {
+void send_key(FILE *hid_dev, unsigned short key, unsigned short mod)
+{
     if (hid_dev != NULL)
     {
-	fprintf (hid_dev, "%c%c%c%c%c%c%c%c", mod, '\0', key, '\0', '\0', '\0', '\0', '\0');
+        fprintf(hid_dev, "%c%c%c%c%c%c%c%c", mod, '\0', key, '\0', '\0', '\0', '\0', '\0');
     }
     else
     {
-        printf(stderr, "Cannot write to device\n" );
+        printf(stderr, "Cannot write to device\n");
     }
 }
 
-enum errors send_unicode (FILE* hid_dev, unsigned int unicode, enum uni_m method, enum kbdl layout) {
-	char buf[10];
-	struct keysym* s;
-	struct layout* l;
+enum errors send_unicode(FILE *hid_dev, unsigned int unicode, enum uni_m method, enum kbdl layout)
+{
+    char buf[10];
+    struct keysym *s;
+    struct layout *l;
 
-	if (unicode == 0x00) {
-		fprintf (stderr, "Symbol not in lookup table!\n");
-		return ERR_SYMBOL;
-	}
+    if (unicode == 0x00)
+    {
+        fprintf(stderr, "Symbol not in lookup table!\n");
+        return ERR_SYMBOL;
+    }
 
-	switch (method) {
-	case SKIP:
-		break;
-	case GTK_HOLD:
-		sprintf (buf, "%x", unicode);
-		s = toscan ("u");
-		l = tolay (s, layout);
-		send_key (hid_dev, l->key, MOD_LCTRL | MOD_LSHIFT);
-		for (int i = 0; i < strlen (buf); i++) {
-			s = toscan ((char[2]){buf[i], '\0'});
-			l = tolay (s, layout);
-			send_key (hid_dev, l->key, MOD_LCTRL | MOD_LSHIFT);
-		}
-		send_key (hid_dev, '\0', '\0');
-		break;
-	case GTK_SPACE:
-		sprintf (buf, "%x ", unicode);
-		s = toscan ("u");
-		l = tolay (s, layout);
-		send_key (hid_dev, l->key, MOD_LCTRL | MOD_LSHIFT);
-		for (int i = 0; i < strlen (buf); i++) {
-			s = toscan ((char[2]){buf[i], '\0'});
-			l = tolay (s, layout);
-			send_key (hid_dev, l->key, MOD_NONE);
-		}
-		send_key (hid_dev, '\0', '\0');
-		break;
-	case WINDOWS:
-		fprintf (stderr, "windows method not implemented!\n");
-		return ERR_LAZY;
-	default:
-		fprintf (stderr, "unknown unicode method!\n");
-		return ERR_LAYOUT; //TODO: better error code
-	}
-	return ERR_SUCCESS;
+    switch (method)
+    {
+    case SKIP:
+        break;
+    case GTK_HOLD:
+        sprintf(buf, "%x", unicode);
+        s = toscan("u");
+        l = tolay(s, layout);
+        send_key(hid_dev, l->key, MOD_LCTRL | MOD_LSHIFT);
+        for (int i = 0; i < strlen(buf); i++)
+        {
+            s = toscan((char[2]){buf[i], '\0'});
+            l = tolay(s, layout);
+            send_key(hid_dev, l->key, MOD_LCTRL | MOD_LSHIFT);
+        }
+        send_key(hid_dev, '\0', '\0');
+        break;
+    case GTK_SPACE:
+        sprintf(buf, "%x ", unicode);
+        s = toscan("u");
+        l = tolay(s, layout);
+        send_key(hid_dev, l->key, MOD_LCTRL | MOD_LSHIFT);
+        for (int i = 0; i < strlen(buf); i++)
+        {
+            s = toscan((char[2]){buf[i], '\0'});
+            l = tolay(s, layout);
+            send_key(hid_dev, l->key, MOD_NONE);
+        }
+        send_key(hid_dev, '\0', '\0');
+        break;
+    case WINDOWS:
+        fprintf(stderr, "windows method not implemented!\n");
+        return ERR_LAZY;
+    default:
+        fprintf(stderr, "unknown unicode method!\n");
+        return ERR_LAYOUT; //TODO: better error code
+    }
+    return ERR_SUCCESS;
 }
